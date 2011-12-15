ea09a5f40781a94115a35bc46cf3e785268059d8
Amy Ousterhout <aousterh@princeton.edu>
2011-12-15 00:05:30 -0500

    removing extlibs from git

--- haggle/android/extlibs/obj/local/armeabi/objs/crypto/ebcdic.o.d
+++
@@ -1,8 +0,0 @@
-/Users/Amy/Documents/Junior_Year/COS_397/Haggle_stuff/checked_out/haggle/android/extlibs/obj/local/armeabi/objs/crypto/ebcdic.o: \
- /Users/Amy/Documents/Junior_Year/COS_397/Haggle_stuff/checked_out/haggle/android/extlibs/external/openssl/crypto/ebcdic.c \
- external/openssl/include/openssl/e_os2.h \
- external/openssl/include/openssl/opensslconf.h
-
-external/openssl/include/openssl/e_os2.h:
-
-external/openssl/include/openssl/opensslconf.h:
--- haggle/android/extlibs/obj/local/armeabi/objs/crypto/sha/sha_dgst.o.d
+++
@@ -1,5 +0,0 @@
-/Users/Amy/Documents/Junior_Year/COS_397/Haggle_stuff/checked_out/haggle/android/extlibs/obj/local/armeabi/objs/crypto/sha/sha_dgst.o: \
- /Users/Amy/Documents/Junior_Year/COS_397/Haggle_stuff/checked_out/haggle/android/extlibs/external/openssl/crypto/sha/sha_dgst.c \
- external/openssl/include/openssl/opensslconf.h
-
-external/openssl/include/openssl/opensslconf.h:
